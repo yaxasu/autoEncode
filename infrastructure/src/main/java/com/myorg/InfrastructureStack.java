64e128f60aed70630b644e5a5125b8256731aff8
Neel <patelxneel@outlook.com>
2025-06-10 20:52:38 -0400

    some config for s3 buckets

--- infrastructure/src/main/java/com/myorg/InfrastructureStack.java
+++ infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -1,24 +1,30 @@
 package com.myorg;
 
-import software.constructs.Construct;
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.constructs.Construct;
+import software.amazon.awscdk.services.s3.*;
 
 public class InfrastructureStack extends Stack {
-    public InfrastructureStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
-
     public InfrastructureStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        Bucket sourceBucket = Bucket.Builder.create(this, "AE2SourceBucket")
+                .versioned(true)
+                .lifecycleRules(java.util.List.of(
+                        LifecycleRule.builder()
+                                .enabled(true)
+                                .expiration(Duration.days(90))
+                                .build()))
+                .build();
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "InfrastructureQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Bucket outputBucket = Bucket.Builder.create(this, "AE2OutputBucket")
+                .versioned(false)
+                .cors(java.util.List.of(CorsRule.builder()
+                        .allowedMethods(java.util.List.of(HttpMethods.GET))
+                        .allowedOrigins(java.util.List.of("*"))
+                        .build()))
+                .build();
     }
 }
