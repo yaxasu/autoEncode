2d31326a46aa847ca011f49259a61c6e730250af
Neel <patelxneel@outlook.com>
2025-06-10 20:28:53 -0400

    backend module skeleton

---
+++ backend-jobs/src/main/java/com/autoencode2/backend/App.java
@@ -0,0 +1,13 @@
+package com.autoencode2.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
